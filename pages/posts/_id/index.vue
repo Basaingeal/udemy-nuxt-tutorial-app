9f23ddc9f8d0a77a3b973773ce2ff38e9f710588
Michael Currie <mcurrie@agwsinc.com>
2018-10-09 10:17:38 -0500

    Adding Async Data to a Single Post

--- pages/posts/_id/index.vue
+++ pages/posts/_id/index.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="single-post-page">
     <section class="post">
-      <h1 class="post-title">Title of the Post</h1>
+      <h1 class="post-title">{{ loadedPost.title }}</h1>
       <div class="post-details">
-        <div>Last updated on XXX</div>
-        <div>Written by NAME</div>
+        <div>Last updated on {{ loadedPost.updatedDate }}</div>
+        <div>Written by {{ loadedPost.author }}</div>
       </div>
-      <p>Content of the post</p>
+      <p>{{ loadedPost.content }}</p>
     </section>
     <section class="post-feedback">
       <p>Let me know what you think about the post, send mail to
@@ -15,6 +15,25 @@
     </section>
   </div>
 </template>
+
+<script>
+export default {
+  async asyncData (context) {
+    await new Promise(resolve => setTimeout(resolve, 1000))
+    return {
+      loadedPost: {
+        id: 1,
+        title: `First Post - ID ${context.params.id}`,
+        previewText: 'This is our first post!',
+        author: 'Michael J. Currie',
+        updatedDate: new Date(),
+        content: 'Some dummy text that is not previewText',
+        thumbnail: 'https://d3atagt0rnqk7k.cloudfront.net/wp-content/uploads/2015/10/12000759/your-guide-to-the-perfect-spooky-halloween.jpg'
+      }
+    }
+  }
+}
+</script>
 
 <style scoped>
 .single-post-page {
