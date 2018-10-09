df74110843c1349f4b6fcb9c02c8a39f35c63733
Michael Currie <mcurrie@agwsinc.com>
2018-10-09 10:02:19 -0500

    Assignment 2: time to Practice - Server-Side Data

--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="posts-page">
-    <PostsList/>
+    <PostsList :posts="loadedPosts"/>
   </div>
 </template>
 
@@ -10,6 +10,25 @@ import PostsList from '@/components/Posts/PostsList'
 export default {
   components: {
     PostsList
+  },
+  async asyncData () {
+    await new Promise(resolve => setTimeout(resolve, 1500))
+    return {
+      loadedPosts: [
+        {
+          id: 1,
+          title: 'First Post',
+          previewText: 'This is our first post!',
+          thumbnail: 'https://d3atagt0rnqk7k.cloudfront.net/wp-content/uploads/2015/10/12000759/your-guide-to-the-perfect-spooky-halloween.jpg'
+        },
+        {
+          id: 2,
+          title: 'Second Post',
+          previewText: 'This is our second post!',
+          thumbnail: 'https://images.indianexpress.com/2017/10/halloween_lead_pxabay-759.jpg'
+        }
+      ]
+    }
   }
 }
 </script>
