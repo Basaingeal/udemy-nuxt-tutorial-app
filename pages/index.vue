f89496b9dbe2ff2a3db0b73c11709590b65cec11
Michael Currie <mcurrie@agwsinc.com>
2018-10-09 10:25:59 -0500

    Using Promises in asyncData

--- pages/index.vue
+++ pages/index.vue
@@ -14,26 +14,33 @@ export default {
   components: {
     PostsList
   },
-  asyncData (context, callback) {
-    console.log(context)
-    setTimeout(() => {
-      callback(null, {
-        loadedPosts: [
-          {
-            id: 1,
-            title: 'First Post',
-            previewText: 'This is our first post!',
-            thumbnail: 'https://d3atagt0rnqk7k.cloudfront.net/wp-content/uploads/2015/10/12000759/your-guide-to-the-perfect-spooky-halloween.jpg'
-          },
-          {
-            id: 2,
-            title: 'Second Post',
-            previewText: 'This is our second post!',
-            thumbnail: 'https://images.indianexpress.com/2017/10/halloween_lead_pxabay-759.jpg'
-          }
-        ]
+  asyncData (context) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve({
+          loadedPosts: [
+            {
+              id: 1,
+              title: 'First Post',
+              previewText: 'This is our first post!',
+              thumbnail: 'https://d3atagt0rnqk7k.cloudfront.net/wp-content/uploads/2015/10/12000759/your-guide-to-the-perfect-spooky-halloween.jpg'
+            },
+            {
+              id: 2,
+              title: 'Second Post',
+              previewText: 'This is our second post!',
+              thumbnail: 'https://images.indianexpress.com/2017/10/halloween_lead_pxabay-759.jpg'
+            }
+          ]
+        })
+      }, 1500)
+    })
+      .then(data => {
+        return data
       })
-    }, 1500)
+      .catch(error => {
+        context.error(error)
+      })
   },
   created () {
 
